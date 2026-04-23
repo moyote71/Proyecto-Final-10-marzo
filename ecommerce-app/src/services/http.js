3cdd1826d2a80978409e420d5599761c9d70d9bd
moyote71 <moyote71@gmail.com>
2026-04-23 14:54:34 -0600

    update de /api 3

--- ecommerce-app/src/services/http.js
+++ ecommerce-app/src/services/http.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_URL;
+const API_BASE = process.env.REACT_APP_API_BASE_URL;
 
 if (!API_BASE) {
   console.error("❌ VITE_API_URL NO está definido en producción");
