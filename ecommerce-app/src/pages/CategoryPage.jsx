84280e56744e170128c49b00f53b6b4d051c2809
moyote71 <moyote71@gmail.com>
2026-04-24 14:07:32 -0600

    categoryPage

--- ecommerce-app/src/pages/CategoryPage.jsx
+++ ecommerce-app/src/pages/CategoryPage.jsx
@@ -1,12 +1,9 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import CategoryProducts from "../components/CategoryProducts/CategoryProducts";
-import { CategoryPageStyles } from "./CategoryPageStyles";
 
 export default function CategoryPage() {
     const { slug } = useParams();
-    return (
-        <div className={CategoryPageStyles.wrapper()}>
-            <CategoryProducts categoryId={categoryId} />
-        </div>
-    );
+
+    return <CategoryProducts categoryId={slug} />;
 }
